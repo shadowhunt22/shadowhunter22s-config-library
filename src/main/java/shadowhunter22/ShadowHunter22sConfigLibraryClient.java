3fa21857efa34127b4405cb3585395adef99f09b
kennerhartman <155584978+shadowhunt22@users.noreply.github.com>
2024-09-15 16:20:44 -0600

    initial commit

---
+++ src/main/java/shadowhunter22/ShadowHunter22sConfigLibraryClient.java
@@ -0,0 +1,21 @@
+//
+// Copyright (c) 2024 by ShadowHunter22. All rights reserved.
+// See LICENSE file in the project root for details.
+//
+
+package shadowhunter22;
+
+import net.fabricmc.api.ClientModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ShadowHunter22sConfigLibraryClient implements ClientModInitializer {
+	public static final String MOD_ID = "shadowhunter22s-config-library";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitializeClient() {
+		LOGGER.info("Successfully loaded {}!", MOD_ID);
+	}
+}
